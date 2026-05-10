c1d4f830c5e56c8c0ca9fc2f6df3922c5c88a2e8
njezeoj <njezeojinnaka@gmail.com>
2026-05-10 23:53:17 +0100

    implement OpenAI response and embeddings generation methods in OpenaiProvider

--- src/openaiProvider.js
+++ src/openaiProvider.js
@@ -1,3 +1,6 @@
+import OpenAI from "openai";
+
+
 class OpenaiProvider {
     constructor(apiKey, modelName){
         this.apiKey = apiKey;
@@ -10,5 +13,40 @@ class OpenaiProvider {
         if(!this.modelName){
             throw new Error("OpenAI model name is required");
         }
+
+        this.openai = new OpenAI({apiKey: this.apiKey});
+    }
+
+    async generateResponse(prompt){
+        try {
+             const response = await this.openai.responses.create({
+            model: this.modelName,
+            input: prompt
+            });
+
+            return response.output_text || "No response generated";
+        } catch (error) {
+            console.error("Error generating response from OpenAI provider:", error);
+            throw new Error("Failed to generate response from OpenAI provider", error.message);
+        }
+    }
+
+    async generateEmbeddings(data){
+        try {
+            const response = await this.openai.embeddings.create({
+                model: "text-embedding-3-small",
+                input: data,
+                encoding_format: "float"
+            });
+            
+            const embeddings = response.data.map((e) => e.embedding);
+            return embeddings;
+
+        } catch (error) {
+            console.error("Error generating embeddings from OpenAI provider:", error);
+            throw new Error("Failed to generate embeddings from OpenAI provider", error.message);
+        }
     }
 }
+
+export default OpenaiProvider;
